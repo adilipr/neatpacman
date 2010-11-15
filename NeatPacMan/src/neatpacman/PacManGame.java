dd6a2424b32405a5982a12fb5312aa7d7b7f443f
novorus <novorus@gmail.com>
2010-11-15 04:13:31 +0000

    basic structure of NeatPacMan set up.

---
+++ NeatPacMan/src/neatpacman/PacManGame.java
@@ -0,0 +1,40 @@
+package neatpacman;
+
+import com.anji.tournament.Game;
+import com.anji.tournament.GameResults;
+import com.anji.tournament.PlayerResults;
+import com.anji.tournament.ScoringWeights;
+
+public class PacManGame implements Game
+{
+
+  @Override
+  public GameResults play(PlayerResults contestantResults,
+                          PlayerResults opponentResults)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Class requiredPlayerClass()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getMaxScore(ScoringWeights weights)
+  {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int getMinScore(ScoringWeights weights)
+  {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
---
+++ NeatPacMan/src/neatpacman/PacManPlayer.java
@@ -0,0 +1,22 @@
+package neatpacman;
+
+import com.anji.tournament.Player;
+
+public class PacManPlayer implements Player
+{
+
+  @Override
+  public String getPlayerId()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void reset()
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
