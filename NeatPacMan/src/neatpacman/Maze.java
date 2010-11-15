6d03525262bec9f2de0f526666bbdf6eaf4158ae
novorus <novorus@gmail.com>
2010-11-15 04:33:03 +0000

    interface between training / simulation added.

--- NeatPacMan/src/neatpacman/Maze.java
+++ NeatPacMan/src/neatpacman/Maze.java
@@ -3,6 +3,7 @@ package neatpacman;
 public interface Maze
 {
 
+  // defs of directions
   static final int UP    = 1;
 
   static final int DOWN  = 2;
@@ -11,12 +12,19 @@ public interface Maze
 
   static final int RIGHT = 8;
 
+  // query game state (for neural network inputs)
+  GameState getPacManState();
+
+  GameState getGhostState(int id);
+
+  // move (and thus change game state)
   void movePacMan(int direction);
 
   void moveGhost(int id, int direction);
 
   boolean isGameOver();
 
+  // query final scores (only used after game over, as fitness functions)
   int getPacManScore();
 
   int getGhostScore();
